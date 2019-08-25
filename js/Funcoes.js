646245752dd6939c26caaaf60852eabcb07ed11c
Nícolas Oliveira <cc19193@g.unicamp.br>
2019-08-25 19:33:58 -0300

    Removendo BUGS, para adicionar outros

--- js/Funcoes.js
+++ js/Funcoes.js
@@ -29,33 +29,55 @@ $(document).ready(function(){
     // função do flip importado de bliblioteca https://nnattawat.github.io/flip/
     $(".card").flip({
         axis: 'y',
-        trigger: 'click'
+        //trigger: 'click'
     });
 
-    $(".card").click(function(){
-
+    $(".card").click(function clique()
+    {
+        var acertos = new Array("a");
         click++; 
 
-        if(click == 2)
+        if(click == 2)  
         {
             if(carta1 == $(this).html())
             {
                 alert("ganhou!");
-            }
-            click = 0;
-            for(i = 1; i <= 14; i++)
-            {
-                if($(".card:nth-child(" + i + ")").data("flip-model").isFlipped)
+                for(i = 1; i <= 14; i++)
                 {
-                    $(".card:nth-child(" + i + ")").flip(false);
+                    if($(".card:nth-child(" + i + ")").html() == $(this).html()){
+                        acertos.push(i)
+                        alert(acertos);
+                        $(".card:nth-child(" + i + ")").off("flip");
+                        $(".card:nth-child(" + i + ")").off("click");
+                    }
                 }
             }
+            else
+            {
+                alert(acertos);
+                click = 0;
+                alert("Infelizmente errou.");
+                for(i = 1; i <= 14; i++)
+                {
+                    if(i == acertos[0] || i == acertos[2] || i == acertos[4] || i == acertos[6] || i == acertos[8] || i == acertos[10] || i == acertos[12] || i == acertos[14])
+                    {
+                        return;
+                    }
+                    else if($(".card:nth-child(" + i + ")").data("flip-model").isFlipped)
+                    {
+                        $(".card:nth-child(" + i + ")").flip(false);
+                    }
+                }
+            }
+            click = 0;
         }
         else
         {
             carta1 = $(this).html();
-            $(this).off(".flip");
+            //$(this).off("flip");
+            //$(this).off("click");
         }
+        
     });
 
 
